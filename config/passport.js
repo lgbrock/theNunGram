1ff1ff70e29454e55a728b3ff49ad3c73e111188
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-04-29 23:05:34 -0400

    added passport config

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('./models/User')
+
+// TODO: Set up google cloud console so below code will work 
+module.exports = function (passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+    }))
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+      
+    passport.deserializeUser(function(id, done) {
+      User.findById(id, function(err, user) {
+        done(err, user);
+      });
+    });
+}
