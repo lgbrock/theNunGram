f515191ed032a6c3bdb5458e5ad6b6187e0b3e71
kylemettling <58491427+kylemettling@users.noreply.github.com>
2021-04-30 10:29:14 -0700

    Removed .env and added dotenv.config path
    Removed .env from main project directory and added the path for dotenv.config

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 //Requiring our dependencies
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const connectDb = require('./config/database');
-const homeRoutes = require('./routes/home');
-const usersRoutes = require('./routes/users');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDb = require("./config/database");
+const homeRoutes = require("./routes/home");
+const usersRoutes = require("./routes/users");
 
 // initialize express
 const app = express();
 
-//Load config
-dotenv.config();
+//Load config setting the .env path to /config/.env
+dotenv.config({ path: "./config/.env" });
 
 // connect to database
 connectDb();
 
 //Calling our packages
-app.set('view engine', 'ejs'); // sets the view engine to render our ejs
-app.use(express.static('public')); // tells express to serve up these 'static' files
+app.set("view engine", "ejs"); // sets the view engine to render our ejs
+app.use(express.static("public")); // tells express to serve up these 'static' files
 app.use(cors()); //allows for cross origin resource sharing(Not used for this project but very important so make it a habit)
-app.use(morgan('tiny')); //logging middleware. Check console for logs
+app.use(morgan("tiny")); //logging middleware. Check console for logs
 
 //body-parsing middleware (required to parse incoming JSON)
 app.use(express.json());
@@ -31,9 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 // // require('dotenv').config({path: './config/.env'})
 
 // // Routes
-app.use('/', homeRoutes);
-app.use('/users', usersRoutes);
-
+app.use("/", homeRoutes);
+app.use("/users", usersRoutes);
 
 //Initializing our PORT
 let PORT = process.env.PORT;
