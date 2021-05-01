27e59e0140f4e4408bd72d89b952183453b168f6
WON-DER <wonderfilledyt@gmail.com>
2021-04-30 19:37:52 -0700

    added both addPost and posts view/controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,33 @@
-module.exports  = {
-    getPosts: (req,res) => {
-        res.render('posts.ejs')
-    },
-    randomPage: (req, res) => {
-        res.render('randomPage.ejs')
+const Post = require("../models/Post");
+
+module.exports = {
+  getPosts: async (req, res) => {
+    try {
+      const posts = await Post.find();
+      console.log(posts);
+      res.render("posts.ejs", { posts });
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+  randomPage: (req, res) => {
+    res.render("randomPage.ejs");
+  },
+  addPost: (req, res) => {
+    res.render("addPost.ejs");
+  },
+  createPost: async (req, res) => {
+    try {
+      const post = await req.body;
+      console.log(post);
+      await Post.create({
+        caption: post.caption,
+        content: post.content,
+      });
+      console.log(`Post has been added!`);
+      res.redirect("/posts");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
