daf8d31b167458b1936840307f25081e0f4a7bfb
WON-DER <wonderfilledyt@gmail.com>
2021-05-03 10:51:15 -0700

    fixed single post ejs to render with resolved Post find

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,20 +1,20 @@
-const cloudinary = require('../middleware/cloudinary');
-const Post = require('../models/Post');
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
 
 module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render('post.ejs');
+      res.render("post.ejs", { post });
     } catch (err) {
       console.log(err);
     }
   },
   randomPage: (req, res) => {
-    res.render('randomPage.ejs');
+    res.render("randomPage.ejs");
   },
   addPost: (req, res) => {
-    res.render('addPost.ejs');
+    res.render("addPost.ejs");
   },
   createPost: async (req, res) => {
     try {
@@ -30,8 +30,8 @@ module.exports = {
         user: req.user.id,
         author: req.user.displayName,
       });
-      console.log('Post has been added');
-      res.redirect('/profile');
+      console.log("Post has been added");
+      res.redirect("/profile");
     } catch (err) {
       console.log(err);
     }
