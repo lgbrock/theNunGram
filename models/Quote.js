fcc01fe05b957b880fe82c81553b18acf2c4ea4f
Justin V <justinv4994@gmail.com>
2021-05-06 18:22:18 -0400

    contionally render posts/ get random quote

--- models/Quote.js
+++ models/Quote.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const QuoteSchema = new mongoose.Schema({
-  author: {
-    type: String,
-  },
   quote: {
     type: String,
   },
+  contributedBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+  },
 });
 
-module.exports = mongoose.model("Quote", QuoteSchema);
+module.exports = mongoose.model('Quote', QuoteSchema);
